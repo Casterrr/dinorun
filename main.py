eb2f9aeb65f96740fb9ecc4d453717127a22c287
Lucas Matheus Vieira Lúcio <lucmvl2003@gmail.com>
2022-12-10 12:37:14 -0300

    Melhorando a semântica do código
    Neste commit foram removidos e adicionados alguns comentários. Além disso, houve algumas mudanças nos nomes de algumas variáveis para melhorar a semântica do código, deixando os nomes de algumas funções/variáveis mais coerentes com o que elas fazem/são.

--- main.py
+++ main.py
@@ -14,30 +14,34 @@ from positions import *
 from rectangles import *
 
 
-def obstacle_movement(obstacle_list):
+def object_movement(object_list):
     #se há algo na lista
-    if obstacle_list:
-        for obstacle_rect in obstacle_list:
-            # obstacle_rect.x -= 5
+    if object_list:
+        for object_rect in object_list:
 
-            if obstacle_rect.bottom == 315:
-                obstacle_rect.x -= 5
-                tela.blit(stone_image, obstacle_rect)
+            '''
+            Verifica se o objeto é um obstáculo ou uma moeda, para desenhar em tela suas respectivas imagens com seus retângulos.
+            Também define a velocidade com a qual cada objeto irá andar para a esquerda
+            '''
+            if object_rect.bottom == 210:
+                object_rect.x -= 2
+                tela.blit(coin_image, object_rect)
             else:
-                obstacle_rect.x -= 2
-                tela.blit(coin_image, obstacle_rect)
+                object_rect.x -= 5
+                tela.blit(stone_image, object_rect)
+                
         
         '''
         Para cada obstáculo na lista, verifica se a posição X dele é maior que 0. Se sim, retorna esse obstáculo.
         O objetivo aqui, é descartar todos os objetos que já saíram da tela a 100px pelo lado esquerdo.
         '''
-        obstacle_list = [obstacle for obstacle in obstacle_list if obstacle.x > -100]
+        object_list = [obstacle for obstacle in object_list if obstacle.x > -100]
         
-        return obstacle_list
+        return object_list
     # No início do jogo, não haverá nada na lista de obstáculos, pois só são adicionados obstáculos na lista a cada 1.4 segundos
     else: return []
 
-obstacle_rect_list = []
+object_rect_list = []
 
 
 def draw_images ():
@@ -71,12 +75,16 @@ gravity = int()
 
 def has_collided_with_obstacles(dino_rect, obstacle_rectangles):
     '''
-    Finaliza o jogo se houver colisão entre o dinossauro e um obstáculo.
+    Finaliza o jogo se houver colisão entre o dinossauro e um obstáculo. Mas se houver colisão entre o dinossauro e uma moeda, 
+    faz a moeda desaparecer.
     '''
     if obstacle_rectangles:
         for obstacle_rect in obstacle_rectangles:
             if dino_rect.colliderect(obstacle_rect):
-                # print(obstacle_rect.bottom)
+                '''
+                Se a colisão for com uma moeda, remove da lista de obstáculos, fazendo ela desaparecer.
+                E se a colisão não for com uma moeda, retorna "True" para informar que houve colisão com um obstáculo.
+                '''
                 if obstacle_rect.bottom == 210:
                     obstacle_rectangles.remove(obstacle_rect)
                     return False
@@ -85,6 +93,7 @@ def has_collided_with_obstacles(dino_rect, obstacle_rectangles):
                     
                 
     return False
+
 def restart_game(obstacle_rectangles):
     obstacle_rectangles = []
     return obstacle_rectangles
@@ -105,8 +114,8 @@ def show_game_over_screen(game_over_text):
     tela.blit(restart_text, (190, 370))
     py.display.flip()
 
-obstacle_timer = py.USEREVENT + 1
-py.time.set_timer(obstacle_timer, 1400)
+object_timer = py.USEREVENT + 1
+py.time.set_timer(object_timer, 1400)
 
 #While loop para exibição de tela inicial do jogo.
 while True:
@@ -137,26 +146,22 @@ while True:
                     gravity = -20
         
         if event.type == py.KEYDOWN:
-            if has_collided_with_obstacles(dino_rectangle, obstacle_rect_list):
-                obstacle_rect_list = restart_game(obstacle_rect_list)
+            if has_collided_with_obstacles(dino_rectangle, object_rect_list):
+                object_rect_list = restart_game(object_rect_list)
         
         '''
         Adiciona o obstáculo pedra na lista de obstáculos que serão desenhados na tela após certo período de tempo.
         Esses obstáculos serão desenhados fora da tela, do lado direito, com posição X aleatória.
         '''
-        if event.type == obstacle_timer and not has_collided_with_obstacles(dino_rectangle, obstacle_rect_list):
+        if event.type == object_timer and not has_collided_with_obstacles(dino_rectangle, object_rect_list):
             if randint(0,2):
 
-                obstacle_rect_list.append(stone_image.get_rect(midbottom = (randint(900, 1100), stone_y_pos)))
+                object_rect_list.append(stone_image.get_rect(midbottom = (randint(900, 1100), stone_y_pos)))
             else:
-                obstacle_rect_list.append(coin_image.get_rect(midbottom = (randint(900, 1100), coin_y_pos)))
-
-            # print("test")
+                object_rect_list.append(coin_image.get_rect(midbottom = (randint(900, 1100), coin_y_pos)))
 
 
-    # collect_coin(obstacle_rect_list, coin_y_pos)
-
-    if not has_collided_with_obstacles(dino_rectangle, obstacle_rect_list):
+    if not has_collided_with_obstacles(dino_rectangle, object_rect_list):
         draw_images()
     
         #Efeito de pulo do dinossauro na tela.
@@ -164,7 +169,7 @@ while True:
         gravity += 1
 
         # Obstacle movement
-        obstacle_rect_list = obstacle_movement(obstacle_rect_list)
+        object_rect_list = object_movement(object_rect_list)
 
         #Atualiza todos os componentes da tela.
         py.display.flip()
